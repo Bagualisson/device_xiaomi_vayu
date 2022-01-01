3ccc6cd611a2f0e2be43fa083f3f7d34c7993559
AlissonGrizotti <alisson1397@hotmail.com>
2022-01-01 15:17:04 +0000

    vayu: Havoc-Ify

--- havoc_vayu.mk
+++ havoc_vayu.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := havoc_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
