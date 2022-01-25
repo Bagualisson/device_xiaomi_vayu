fb5ffa4ab191b8d29feb58c7220c3324216017cf
AlissonGrizotti <alisson1397@hotmail.com>
2022-01-25 02:28:08 +0000

    vayu: Ancient Bringup

--- ancient_vayu.mk
+++ ancient_vayu.mk
@@ -12,11 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+ANCIENT_OFFICIAL ?= true
+ANCIENT_GAPPS ?= true
+TARGET_USES_BLUR := true
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := ancient_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
