1bc1aa59ec4a941550a975aeff270cae710a9d36
johnmart19 <johnivan19999@gmail.com>
2022-03-30 19:48:25 +0300

    vayu: Build missing bluetooth libs
    E a2dp_offload: open_a2dp_sink: DLOPEN failed for libbthost_if_sink.so
    
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    libbthost_if \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
