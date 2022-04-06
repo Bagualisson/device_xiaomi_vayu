95c66fc6f2e2ea376b950fbdc6d4c540c2cc9cbb
AlissonGrizotti <alisson1397@hotmail.com>
2022-04-06 02:36:12 +0000

    vayu: Drop Camera2 and GcamGo prebuilt

--- device.mk
+++ device.mk
@@ -103,8 +103,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    GCamGOPrebuilt \
-    libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
