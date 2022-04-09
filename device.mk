1c429297f9efe214d6bb8f47ff165fad0e3c6059
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-04-09 21:10:53 +0000

    vayu: Build vendor.qti.hardware.capabilityconfigstore@1.0.vendor F linker : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
     libldacBT_bco
 
 PRODUCT_COPY_FILES += \
