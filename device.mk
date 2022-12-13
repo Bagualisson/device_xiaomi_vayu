7a17284dc3ec4ce2caddff22cde24cc8c23a94a4
EndCredits <alicization.han@gmail.com>
2022-12-13 14:37:27 +0800

    vayu: Build QTI Perf hal
    12-13 13:57:05.373  4189  4189 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.iop@2.0-service": library "vendor.qti.hardware.perf@2.2.so" not found: needed by /vendor/lib64/libqti-perfd-client.so in namespace (default)
    12-13 13:57:05.379  4190  4190 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.perf@2.2-service": library "vendor.qti.hardware.perf@2.0.so" not found: needed by main executable
    
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -388,6 +388,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
     vendor.qti.hardware.perf@2.2.vendor \
     android.hardware.power@1.1 \
     android.hardware.power@1.2 \
