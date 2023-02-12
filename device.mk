b239807bd1ec94061634005e9ca447ed836a88de
Bagualisson <alisson1397@hotmail.com>
2023-02-12 10:58:21 -0300

    vayu: Inherit MiuiCamera

--- device.mk
+++ device.mk
@@ -376,6 +376,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# MiuiCamera
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
